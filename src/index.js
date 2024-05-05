4f64454434816557dff12dfae339f1a7ba950afa
AleksandrPetrov89 <petrovav89@gmail.com>
2024-05-05 21:29:51 +0700

    Fixed the test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import orderByProps from './js/app.js';
-
-// For example
-const person = {
-    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-};
-console.log(orderByProps(person, [
-    'name', 'level',
-]));
-console.log(orderByProps(person));
